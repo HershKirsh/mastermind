40ad086f8e2ae9a58c37bfa7bdccea4c028f8276
hershkirsh <hershkirsh@gmail.com>
2020-10-06 09:58:42 -0400

    bug fixes

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ const gameElems = {
             })
             elem.addEventListener('dragleave', (e) => {
                 if (elem.parentElement.classList.contains('active')) {
-                    dragElems.removePeg(e, elem);
+                    dragElems.removePeg(elem);
                 }
             })
         });
@@ -45,69 +45,68 @@ const gameElems = {
             if (!this.s.includes(color)) return
             if ((!match.white.includes(color) && !match.red.includes(color)) || (this.s.filter(x => x === color).length > (match.white.filter(x => x === color).length + match.red.filter(x => x === color).length))) {
                 if (this.s[i] === color) {
-                    match.white.push(color);
-                } else {
                     match.red.push(color);
-                }
-            } else if (match.red.includes(color)) {
-                if (this.s[i] === color) {
-                    match.red.splice(match.red.indexOf(color), 1);
+                } else {
                     match.white.push(color);
+                }
+            } else if (match.white.includes(color)) {
+                if (this.s[i] === color) {
+                    match.white.splice(match.red.indexOf(color), 1);
+                    match.red.push(color);
                 }
             }
         });
-        match.white.forEach(x => {
-            htmlElements.rows[row].querySelector('.marking-hole:not(.taken)').classList.add('white-mark', 'taken');
-        });
         match.red.forEach(x => {
             htmlElements.rows[row].querySelector('.marking-hole:not(.taken)').classList.add('red-mark', 'taken');
+        });
+        match.white.forEach(x => {
+            htmlElements.rows[row].querySelector('.marking-hole:not(.taken)').classList.add('white-mark', 'taken');
         });
     }
 }
 
 const dragElems = {
     draggedColor: '',
-    currentPeg: '',
     activateColors: function () {
         htmlElements.colorPegs.forEach(elem => {
-            // funArray[currentRow][currentPeg].drag = dragElems.drag
-            // elem.addEventListener('drag', (e) => {
-            //     dragElems.drag(elem)
-            // })
+            elem.draggable = true;
             elem.addEventListener('drag', dragElems.drag)
             elem.addEventListener('dragend', (e) => {
                 dragElems.endDrag(e, elem)
             })
         })
-
     },
     drag: function () {
         dragElems.draggedColor = this.id;
     },
-    endDrag: function (e, elem) {
-        this.draggedColor = '';
-        this.filledHole = '';
-        if (gameElems.placedPegCount === 4) {
-            gameElems.markRow(gameElems.currentRow)
-            gameElems.activateRow();
-        }
-    },
     addPeg: function (e, elem) {
-        if (this.draggedColor) {
-            console.log(this.draggedColor);
-            elem.classList.contains('filled') ? elem.classList.add('changed') : gameElems.placedPegCount++;
+        if (this.draggedColor && !elem.classList.contains('filled')) {
+            elem.classList.add(this.draggedColor);
+            gameElems.placedPegCount++;
             this.filledHole = elem;
-            elem.classList.add(this.draggedColor, 'filled');
-            this.currentPeg = elem;
         }
     },
     keepPeg: function (e, elem) {
-        if (this.draggedColor) elem.classList.add(this.draggedColor);
+        if (this.draggedColor && !elem.classList.contains('filled')) elem.classList.add(this.draggedColor);
     },
-    removePeg: function (e, elem) {
-        elem.classList.contains('changed') ? elem.classList.remove('changed') : gameElems.placedPegCount--;
-        if (this.draggedColor && elem === this.filledHole) {
-            elem.classList.remove(this.draggedColor, 'filled');
+    removePeg: function (elem, removing) {
+        if (elem.parentElement.classList.contains('active') && ((this.draggedColor && elem === this.filledHole) || removing)) {
+            elem.classList = 'peg-hole';
+            this.filledHole = '';
+            gameElems.placedPegCount--;
         }
-    }
+    },
+    endDrag: function (e, elem) {
+        if (this.filledHole) {
+            this.filledHole.classList.add('filled');
+            const thisPeg = this.filledHole;
+            this.filledHole.addEventListener('click', () => this.removePeg(thisPeg, true))
+            this.draggedColor = '';
+            this.filledHole = '';
+            if (gameElems.placedPegCount === 4) {
+                gameElems.markRow(gameElems.currentRow)
+                gameElems.activateRow();
+            }
+        }
+    },
 }
